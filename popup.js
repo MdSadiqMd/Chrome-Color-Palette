3a74f85097dee45767e82594ff356a149e12bda8
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-29 17:33:47 +0530

    refactor added color manipulationusing eyeDropper

--- popup.js
+++ popup.js
@@ -1,43 +1,39 @@
 const btn = document.querySelector('.changeColorBtn');
 const colorGrid = document.querySelector('.colorGrid');
 const colorValue = document.querySelector('.colorValue');
-let tab; // Define the tab variable in the outer scope
 
 btn.addEventListener('click', async () => {
-    // Get the active tab within the click event handler
-    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-        tab = tabs[0]; // Assign the tab value here
-        if (tab) {
-            chrome.scripting.executeScript(
-                {
-                    target: { tabId: tab.id },
-                    function: pickColor,
-                },
-                async (injectionResults) => {
-                    const [data] = injectionResults;
-                    if (data.result) {
-                        const color = data.result.sRGBHex;
-                        colorGrid.style.backgroundColor = color;
-                        colorValue.innerText = color;
-                        try {
-                            await navigator.clipboard.writeText(color);
-                        } catch (err) {
-                            console.error(err);
-                        }
-                    }
-                }
-            );
-        }
+    chrome.storage.sync.get('color', ({ color }) => {
+        console.log('color: ', color);
     });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: tab.id },
+            function: pickColor,
+        },
+        async (injectionResults) => {
+            const [data] = injectionResults;
+            if (data.result) {
+                const color = data.result.sRGBHex;
+                colorGrid.style.backgroundColor = color;
+                colorValue.innerText = color;
+                try {
+                    await navigator.clipboard.writeText(color);
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        }
+    );
 });
 
 async function pickColor() {
     try {
+        // Picker
         const eyeDropper = new EyeDropper();
-        const result = await eyeDropper.open();
-        if (result) {
-            return { result: { sRGBHex: result } };
-        }
+        return await eyeDropper.open();
     } catch (err) {
         console.error(err);
     }
